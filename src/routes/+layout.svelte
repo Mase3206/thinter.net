ca975cde086c5f9b2c581525f8f33929c8c10eda
Mase3206 <35052448+Mase3206@users.noreply.github.com>
2025-07-27 14:36:36 -0600

    fix flowbite

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
-	import '$lib/main.css';
+	import '../app.css';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from "flowbite-svelte";
 	
 	let { children } = $props();
 </script>
+
+
+<Navbar>
+	<NavBrand href="/">
+		<span class="self-center text-xl font-semibold-whitespace-nowrap dark:text-white">The Thinternet</span>
+	</NavBrand>
+
+	<NavHamburger />
+
+	<NavUl>
+		<NavLi href="#">About</NavLi>
+	</NavUl>
+</Navbar>
 
 {@render children()}
